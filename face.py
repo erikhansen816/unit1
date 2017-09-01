fde1358f78107e02d47328b2e78dbc7b8e964ae7
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-01 14:36:19 -0400

    Updated from Brython Server: 9/1/2017 2:36:19 PM

--- face.py
+++ face.py
@@ -1,1 +1,12 @@
+#Erik Hansen
+#9/1/2017
+#face.py - Making a face on python
+
+print('     xxxxx     ')
+print('    x     x    ')
+print('((    o o    ))')
+print('   |   v   |   ')
+print('   |  ===  |   ')
+print('     -----     ')
+
 
