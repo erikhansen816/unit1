c3f6f016b68349c9e9355ebb476e9830db9cd594
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-06 13:48:27 -0400

    Updated from Brython Server: 9/6/2017 1:48:22 PM

--- tipCalculator.py
+++ tipCalculator.py
@@ -1,1 +1,7 @@
+#Erik Hansen
+#9/6/2017
+#tipCalculator.py - Shows amount to tip
 
+price = float(input('Price of meal (in dollars): '))
+tip = float(input('% to tip: '))
+print('You should tip ', price*(tip/100),'dollars')
