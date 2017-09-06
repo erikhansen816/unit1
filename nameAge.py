d16e539503419f291eb583eeeafb321827b95f6f
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-06 13:44:41 -0400

    Updated from Brython Server: 9/6/2017 1:44:35 PM

--- nameAge.py
+++ nameAge.py
@@ -3,8 +3,8 @@
 #ageName.py - showing letters in name and next age
 
 name = input('Enter your first and last name: ')
-age = input('Enter your age: ')
-firstname, lastname = name.split
+age = int(input('Enter your age: '))
+firstname, lastname = name.split()
 print('Your first name has', len(firstname), 'letters')
-print('Your lasr name has', len(lastname), 'letters')
+print('Your last name has', len(lastname), 'letters')
 print('Next year you will be', age+1, 'years old')
