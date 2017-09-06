62db7c8adab39608ce59aa0b7d239c0500c3ce6e
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-06 13:59:17 -0400

    Updated from Brython Server: 9/6/2017 1:59:11 PM

--- programSlope.py
+++ programSlope.py
@@ -1,1 +1,8 @@
+#Erik Hansen
+#9/6/2017
+#programSlope.py - finding slope and equation of line
 
+x1 = float(input('x1: ')
+x2 = float(input('x2: ')
+y1 = float(input('y1: ')
+y2 = float(input('y2: ')
