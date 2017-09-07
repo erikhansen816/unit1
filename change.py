68f19269acc73c5f10138a55767ae71b0433dcc5
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-07 14:51:00 -0400

    Updated from Brython Server: 9/7/2017 2:50:53 PM

--- change.py
+++ change.py
@@ -1,1 +1,16 @@
+#Erik Hansen
+#9/7/2017
+#change.py - cents to different coins
 
+cents = int(input('Input a number of cents: '))
+quarters = cents//25
+remainder1= cents%25
+dimes = remainder1//10
+remainder2 = remainder1%10
+nickels = remainder2//5
+remainder3 = remainder2%5
+pennies = remainder3//1
+print('Quarters: ',quarters)
+print('Dimes: ',dimes)
+print('Nickels: ',nickels)
+print('Pennies: ',pennies)
