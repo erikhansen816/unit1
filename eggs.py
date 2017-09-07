a5d0a8ae3abc02479590d37350b4ca64c1e33cb7
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-07 14:46:08 -0400

    Updated from Brython Server: 9/7/2017 2:46:01 PM

--- eggs.py
+++ eggs.py
@@ -1,1 +1,7 @@
+#Erik Hansen
+#9/7/2017
+#eggs.py - counts number of eggs and cartons
 
+eggs = int(input('Input number of eggs: '))
+cartons = int(input('Input number of egg cartons: '))
+print(eggs/cartons<=12)
