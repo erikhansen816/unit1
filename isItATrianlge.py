7caa09fbba03d5b9ba4e5d026dcfde189beecd16
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-08 14:38:11 -0400

    Updated from Brython Server: 9/8/2017 2:38:03 PM

--- isItATrianlge.py
+++ isItATrianlge.py
@@ -6,4 +6,8 @@ sidea = float(input('Enter side A: '))
 sideb = float(input('Enter side B: '))
 sidec = float(input('Enter side C: '))
 
+smallside = min(sidea,sideb,sidec)
+bigside = max(sidea,sideb,sidec)
+middleside = sidea+sideb+sidec-(smallside+bigside)
+print(middleside+smallside>bigside)
 
