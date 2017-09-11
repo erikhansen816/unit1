8ce3c18d4c9e89ad0bfdd54b539f3616c0f4d560
erikhansen816 <31485788+erikhansen816@users.noreply.github.com>
2017-09-11 14:20:33 -0400

    Updated from Brython Server: 9/11/2017 2:20:23 PM

--- quiz1.py
+++ quiz1.py
@@ -1,1 +1,10 @@
+#Erik Hansen
+#9/11/2017
+#quiz1.py - First quiz, names and numbers
 
+print('Erik Hansen')
+number1 = int(input('Enter a number: '))
+number2 = int(input('Enter another number: '))
+print('The sum is',number1+number2)
+print('The product is',number1*number2)
+print('Your lucky number is',number1*7+number2*7)
